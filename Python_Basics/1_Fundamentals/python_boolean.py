b4695a0d00639e864a283827f342e0dba5e5e650
Ashwin Sawant <100dollarsinbank@gmail.com>
2025-10-27 19:01:49 +0530

    add python_boolean python file

---
+++ Python_Basics/1_Fundamentals/python_boolean.py
@@ -0,0 +1,45 @@
+# In programming, you often want to check if a condition is true or not and perform some actions based on the result.
+# To represent true and false, Python provides you with the boolean data type. The boolean value has a technical name as bool.
+
+# The boolean data type has two values: True and False.
+
+is_active = True
+is_admin = False
+
+# When you compare two numbers, Python returns the result as a boolean value.
+x = 20
+y = 10
+
+print(x > y)        # True
+print(x < y)        # False
+
+# Also, comparing two strings results in a boolean value:
+x = 'a'
+y = 'b'
+
+print(x > y)        # False
+print(x < y)        # True
+
+
+# The bool() function
+# To Find out if a value is True or False, you use the bool() function.
+
+result = bool("HI")
+print(result)       # True
+
+result = bool(100)
+print(result)       # True
+
+result = bool(0)
+print(result)       # False
+
+
+# Falsy and Truthy Values
+# when a value evaluates to True, it's truthy. If a value evaluates to False, it's falsy.
+# The following are falsy values in Python
+# Flasy values are the number zero, an empty string, False, None, an empty list [], an empty tuple (), and an empty dictionary {}.
+
+# The truthy values are the other values that aren't falsy.
+
+# None, list, tuple, and dictionary -- later in the upcomming tutorials.
+
