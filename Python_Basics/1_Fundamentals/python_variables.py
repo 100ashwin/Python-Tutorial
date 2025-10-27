5c9a172a8c529258b744d2a9e09861942b4bee9d
Ashwin Sawant <100dollarsinbank@gmail.com>
2025-10-27 18:12:55 +0530

    add python_variables python file

---
+++ Python_Basics/1_Fundamentals/python_variables.py
@@ -0,0 +1,22 @@
+message = "Hello, World!"
+print(message)      # Hello, World!
+
+message = "Good Bye!"
+print(message)      # Good Bye!
+
+message = 784343
+print(message)      # 784343
+
+
+# The following are the variable rules that you should keep in mind:
+
+# Variable names can contain only letters, numbers, and underscores (_). They can start with a letter or an underscore (_), not with a number.
+# Variable names cannot contain spaces. To separate words in variables, you use underscores, for example sorted_list.
+# Variable names cannot be the same as keywords, reserved words, and built-in functions in Python.
+
+
+# Summary:
+
+# A variable is a label that you can assign a value to it. The value of a variable can change throughout the program.
+# Use the variable_name = value to create a variable.
+# The variable names should be as concise and descriptive as possible. Also, they should adhere to Python variable naming rules.
