1b1d8b6706a3d19047e2a3e5d1893dfda61105ec
Ashwin Sawant <100dollarsinbank@gmail.com>
2025-10-28 15:51:34 +0530

    add python_assignment_operators python file

---
+++ Python_Basics/2_Operators/py_assignment_operator.py
@@ -0,0 +1,34 @@
+# Python Assignment Operators
+# In Python, you use the assignment operator to assign a value to a variable.
+
+count = 0
+print(count)        # 0
+
+count = count + 1
+print(count)        # 1
+
+# Note that the = in the expression is an assignment, 
+# not equal comparison:
+# In Python, the equal operator that compares two values is ==.
+
+
+# Compound Assignment Operators
+
+count = 0
+count += 1          # same as count = count + 1
+print(count)        # 1
+
+# compound assignment opeators in Python:
+x = 5
+y = 2
+
+x += y
+print(x)            # 7
+x -= y
+print(x)            # 5
+
+x **= y
+print(x)            # 25
+
+# and many other arithmetic symbols.....
+
